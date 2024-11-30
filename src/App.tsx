91157d904fb265ee49850d20e06bf288eb5c9f81
Bohdan Voitov <bohdan.voiyov@gmail.com>
2024-11-30 18:08:42 +0200

    Added Toast notifications for new posts and socket integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { AppDispatch } from './store/store';
@@ -12,9 +12,12 @@ import { NewPostPage } from './layouts/NewPostPage';
 import { Container } from '@mui/material';
 import { ProtectedRoute } from './components/ProtectedRoute/ProtectedRoute';
 import { Notification } from './components/Notification/Notification';
+import { Toast } from './components/Toast/Toast';
+import { socketService } from './services/socketService';
 
 const App: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
+  const [toast, setToast] = useState({ open: false, message: '' });
 
   useEffect(() => {    
     const token = localStorage.getItem('token');
@@ -23,10 +26,31 @@ const App: React.FC = () => {
     }
   }, [dispatch]);
 
+  useEffect(() => {
+    const socket = socketService.connect();
+    
+    socket.on('newPost', (data: { message: string }) => {
+      setToast({ open: true, message: data.message });
+    });
+
+    return () => {
+      socketService.disconnect();
+    };
+  }, []);
+
+  const handleCloseToast = () => {
+    setToast({ ...toast, open: false });
+  };
+
   return (
     <Router>
       <ControlBar />
       <Container>
+        <Toast 
+          open={toast.open}
+          message={toast.message}
+          onClose={handleCloseToast}
+        />
         <Notification />
         <Routes>
           <Route path="/" element={<StripePage />} />
