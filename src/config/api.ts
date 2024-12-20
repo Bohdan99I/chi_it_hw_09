9b2bbec2a1c4cbad362353e27633527415b36bc6
Bohdan Voitov <bohdan.voiyov@gmail.com>
2024-12-20 18:07:43 +0200

    fix_api

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,7 +1,7 @@
 export const API_ENDPOINTS = {
     // Auth endpoints
     LOGIN: '/api/auth/login',
-    REGISTER: '/api/users/register',
+    REGISTER: '/users/register',
     GET_PROFILE: '/api/users/my-profile',
     GET_USER: (idOrUsername: string) => `/api/users/${idOrUsername}`,
 
